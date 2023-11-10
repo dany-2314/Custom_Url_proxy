5f9b13c8d652cf32daddabc6a581c3e447a2fcf7
Dany <dshahid980@gmail.com>
2023-11-10 14:37:38 -0500

    reverted some changes

--- render_api.py
+++ render_api.py
@@ -5,37 +5,35 @@ import subprocess
 import os
 
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/', methods=['GET'])
-    def scrape_and_display():
-        url = request.args.get("url")
-        if url is None:
-            return "There is no url, please enter a url to be parsed"
-        
-        # Get the current working directory because we will need to go back after the initial change
-        curr_wd = os.getcwd()
-        os.chdir("myproject/myproject")
+@app.route('/', methods=['GET'])
+def scrape_and_display():
+    url = request.args.get("url")
+    if url is None:
+        return "There is no url, please enter a url to be parsed"
 
-        output_file = "output.xml"
+    # Get the current working directory because we will need to go back after the initial change
+    curr_wd = os.getcwd()
+    os.chdir("myproject/myproject")
 
-        # Check to see if output file exists before
-        # If it does we want to remove it before creating a new one, re-writing does not work
-        if os.path.exists(output_file):
-            os.remove(output_file)
+    output_file = "output.xml"
 
-        subprocess.check_output(['scrapy', 'crawl', "myspider", "-a", f"url={url}", "-o", output_file], text=True)
-        # Extract the HTML data from the spider
-        with open('output.xml', 'r', encoding='utf-8') as f:
-            data = f.read()
+    # Check to see if output file exists before
+    # If it does we want to remove it before creating a new one, re-writing does not work
+    if os.path.exists(output_file):
+        os.remove(output_file)
 
-        # Change back to original path so the initial change path doesnt cause an isse
-        os.chdir(curr_wd)
+    subprocess.check_output(['scrapy', 'crawl', "myspider", "-a", f"url={url}", "-o", output_file], text=True)
+    # Extract the HTML data from the spider
+    with open('output.xml', 'r', encoding='utf-8') as f:
+        data = f.read()
 
-        return data
+    # Change back to original path so the initial change path doesnt cause an isse
+    os.chdir(curr_wd)
 
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run()
+    return data
+   
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
